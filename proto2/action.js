b63a81e40f4709c5127a637bb207ed9ff56c3e0d
antoineluong <luong.antoine@gmail.com>
2011-08-03 18:18:48 +0200

    Ajout de la possibilité de déplacer et réinitialiser le marqueur position

--- proto2/action.js
+++ proto2/action.js
@@ -31,3 +31,57 @@ function soumettre(){
     xhr.open("GET", url, true); 
     xhr.send(null); 
 }
+
+function initCoord(){
+
+    if (google.loader.ClientLocation){
+        coord = locate();       
+    }
+
+    map = new google.maps.Map(document.getElementById("map_canvas"), getOptions(coord));
+
+    position = new google.maps.Marker({
+        position: coord,
+        map: map, 
+        title: "Votre position",
+        icon: "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=O|009900|000000"  
+    }); 
+}
+
+function lireCoord(){
+    var lat = document.getElementById('latitude').value;
+    var lon = document.getElementById('longitude').value; 
+    return new google.maps.LatLng(lat,lon); 
+}
+
+function gotoCoord(){                                 
+    setCoord(lireCoord());
+}
+
+function setCoord(coord){                                 
+    map.setOptions(getOptions(coord));
+    position.setPosition(coord);
+}
+
+function getOptions(coord){                  
+    var options = {
+        zoom: 6,
+        center: coord,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    return options;                  
+}
+
+function resetCoord(){
+    if (google.loader.ClientLocation){
+        setCoord(locate());        
+    }
+}
+
+function locate(){            
+    lat = google.loader.ClientLocation.latitude;
+    lon = google.loader.ClientLocation.longitude;
+    document.getElementById('latitude').value = lat;
+    document.getElementById('longitude').value = lon;
+    return new google.maps.LatLng(lat,lon);       
+}
